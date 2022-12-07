b0ee542a00ed990fba64c619853f8f4e633858f3
bibitdharmawan <115470438+bibitdharmawan@users.noreply.github.com>
2022-12-07 17:41:34 +0700

    php

---
+++ belajar3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php 
+	$mhs_putri	= array("Reni","Rina","Weni","Vina");
+
+	$mhs[0]	= "Budi";
+	$mhs[1]	= "Beni";
+	$mhs[2]	= "Bima";
+	$mhs[3]	= "Bedu";
+	$mhs[4]	= "Beti";
+	for ($i=0; $i <= 3 ; $i++) { 
+		echo "Nama Saya $mhs_putri[$i] <br>";
+	}
+	 ?>
+</body>
+</html>
