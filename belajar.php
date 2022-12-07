4fabade826bbc482cefeab40601c9620ea0de6d3
bibitdharmawan <115470438+bibitdharmawan@users.noreply.github.com>
2022-12-07 17:28:59 +0700

    php

---
+++ belajar.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Belajar PHP</title>
+</head>
+<body>
+	<?php
+		$kata2 = "<div style='color: red'> Bismillah</div>";
+		$kata1 = '<div style="color: red"> Bismillah</div>';
+		$kata = "<div style=\"color: red\"> Bismillah</div>";
+		echo $kata;
+	?>
+</body>
+</html>
